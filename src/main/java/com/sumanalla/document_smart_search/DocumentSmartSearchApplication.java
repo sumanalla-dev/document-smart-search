e0869f99c6457b8992ddd5b9528e3eaeb04cc3a4
salla <sumanalla.dev@gmail.com>
2026-09-20 22:45:32 -0400

    feat: initialize document smart search service

---
+++ src/main/java/com/sumanalla/document_smart_search/DocumentSmartSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sumanalla.document_smart_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSmartSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSmartSearchApplication.class, args);
+	}
+
+}
